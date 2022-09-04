39618421e12b6c934d121ad0a57182a13f1acff8
GhazouaniHichem <ghazouanih68@gmail.com>
2022-09-04 12:55:08 +0100

    Config a VPC

--- vpc.tf
+++ vpc.tf
@@ -12,10 +12,28 @@ data "aws_availability_zones" "avail_zs" {}
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.3"
-
+  // Variables needed to configure vpc module
   name = "eks-vpc"
   cidr = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
   public_subnets = var.public_subnet_cidr_blocks
   azs = data.aws_availability_zones.avail_zs.names
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_dns_hostnames = true
+  
+  tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/elb" = 1
+  }
+
 }
